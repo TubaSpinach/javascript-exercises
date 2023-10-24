b7f67349d014cd915adf4b81cb5047ccd03cecde
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-23 22:03:28 -0400

    solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  let dirtyCalc = (deg-32)*5/9;
+  return parseFloat(dirtyCalc.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  let dirtyCalc = (deg*9/5)+32;
+  return parseFloat(dirtyCalc.toFixed(1));
 };
 
 // Do not edit below this line
