1931fe6be819b8f188ed0ccd1d20bca43f6b9472
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-23 21:49:08 -0400

    solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(aStart, anEnd) {
+    let start = (aStart <= anEnd) ? aStart : anEnd;
+    let end = (anEnd === start) ? aStart : anEnd;
+    let answer = 0;
+    if (aStart < 0 || anEnd < 0) {
+        return 'ERROR';
+    }
+    if (typeof aStart !== "number" || typeof anEnd !== "number") {
+        return 'ERROR';
+    }
+    for(let i = start; i<=end; i++) {
+        answer += i;
+    }
+    return answer;
 };
 
 // Do not edit below this line
