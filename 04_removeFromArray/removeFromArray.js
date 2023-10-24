3ceb1e48b5a66e6b34930d0c05263245984992e6
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-23 21:41:18 -0400

    solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inpArray) {
+    const otherElements = Array.prototype.slice.call(arguments,1)
+    return inpArray.filter((inItem)=> {
+        for (element of otherElements){
+            if (element === inItem) {
+                return false;
+            }
+        }
+        return true;
+    })
 };
 
 // Do not edit below this line
