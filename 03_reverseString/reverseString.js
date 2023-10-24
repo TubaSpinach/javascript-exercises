9ee81ffd8d4836e81522215348e875aea9f2b72e
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-23 21:25:12 -0400

    solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inpStr) {
+    let answer = '';
+    for (let i = inpStr.length;i>=0;i--){
+        answer += inpStr.charAt(i);
+    }
+    return answer;
 };
 
 // Do not edit below this line
