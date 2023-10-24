9ef7a14ccf1573738cf188900bb8806359247454
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-23 21:20:04 -0400

    solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inpStr, repNum) {
+    let answer = inpStr;
+    if (repNum == 0){
+        return '';
+    } else if (repNum < 0) {
+        return 'ERROR'
+    }
+    for(let i = 1; i < repNum; i++) {
+        answer += inpStr;
+    }
+    return answer
 };
 
 // Do not edit below this line
