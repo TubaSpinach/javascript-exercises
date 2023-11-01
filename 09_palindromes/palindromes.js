1745eb37c8f2f15c2b5318c97f4781100eb7b42a
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-11-01 19:46:03 -0400

    feat: allow upper-case letters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let letters = [];
+    let noPunctationAllowed = /[!,. ]/g;
+    
+    let cleanWord = word.toLowerCase().replaceAll(noPunctationAllowed,"");
+    for (letter of cleanWord) {
+        letters.push(letter);
+    }
+    let reverso = letters.slice();
+    letters.reverse();
+    if(reverso.toString() == letters.toString()){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
