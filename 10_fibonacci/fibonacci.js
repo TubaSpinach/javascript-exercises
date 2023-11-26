e80014665cbc83e2e185383d1cc53f71df8fae63
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-11-25 19:07:50 -0500

    solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(ind) {
+    if(ind<0){
+        return "OOPS";
+    }
 
+    let current = 1
+    let prev = 0
+    for(let i = 0; i < ind; i++){
+        current += prev;
+        prev = current - prev;
+    }
+
+    return prev;
 };
 
 // Do not edit below this line
