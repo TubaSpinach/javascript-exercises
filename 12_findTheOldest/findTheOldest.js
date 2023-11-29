eabe48008da36677c47ecf1ea7522fffc270fea3
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-11-28 22:27:27 -0500

    passed 1st test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    ages = people.map((x)=>x.yearOfDeath - x.yearOfBirth)
+    oldestAge = Math.max(...ages)
+    oldestIndex = ages.findIndex((x)=> x == oldestAge)
+    console.log(ages,oldestAge,oldestIndex)
+    return people[oldestIndex]
 };
 
 // Do not edit below this line
